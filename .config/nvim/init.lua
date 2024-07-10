940f274bd00f9e0d3b67e184866ea6d893cae564
Kevin Ren <work.kevin.ren@gmail.com>
2024-07-09 20:42:13 -0700

    NVIM - integrated nvim with vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
-require("kevin.core")
-require("kevin.lazy")
-
+if vim.g.vscode then
+	-- VSCode extension
+	local keymap = vim.keymap -- for conciseness
+	keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+else
+	-- ordinary Neovim
+	require("kevin.core")
+	require("kevin.lazy")
+end
