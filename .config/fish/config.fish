80056780e87b378b9aa8a00041b43f731819613f
Kevin Ren <work.kevin.ren@gmail.com>
2025-03-09 13:06:40 -0700

    FISH - added bang dollar shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,4 +19,29 @@ if status is-interactive
     # fish_vi_key_bindings
     starship init fish | source
     fish_default_key_bindings
+
+
+    function bind_bang
+        switch (commandline -t)[-1]
+            case "!"
+                commandline -t -- $history[1]
+                commandline -f repaint
+            case "*"
+                commandline -i !
+        end
+    end
+
+    function bind_dollar
+        switch (commandline -t)[-1]
+            case "!"
+                commandline -f backward-delete-char history-token-search-backward
+            case "*"
+                commandline -i '$'
+        end
+    end
+
+    function fish_user_key_bindings
+        bind ! bind_bang
+        bind '$' bind_dollar
+    end
 end
