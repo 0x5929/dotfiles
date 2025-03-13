581b60d72e9af89adeaf5f99fe69d7f3893b4d29
Kevin Ren <work.kevin.ren@gmail.com>
2025-03-13 00:55:19 -0700

    QTILE DASHBOARD FISH - updated configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ if status is-interactive
   
     # aliases
     alias vim=nvim
+    alias open=xdg-open
+    alias pycharm=pycharm-professional
     alias xclip="xclip -selection clipboard"
     alias chrome="google-chrome-stable"
     alias dotfiles="/usr/bin/git --git-dir=$HOME/Repositories/personal/dotfiles --work-tree=$HOME"
