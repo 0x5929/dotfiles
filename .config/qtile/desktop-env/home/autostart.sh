9f9e276d6f447f707bc14bcaa82a9d740834be20
Kevin Ren <work.kevin.ren@gmail.com>
2025-09-15 00:00:10 -0700

    QTILE - added home one monitor setup

---
+++ .config/qtile/desktop-env/home/autostart.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#   ___ _____ ___ _     _____   ____  _             _    
+#  / _ \_   _|_ _| |   | ____| / ___|| |_ __ _ _ __| |_  
+# | | | || |  | || |   |  _|   \___ \| __/ _` | '__| __| 
+# | |_| || |  | || |___| |___   ___) | || (_| | |  | |_  
+#  \__\_\|_| |___|_____|_____| |____/ \__\__,_|_|   \__| 
+#                                                        
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+# My screen resolution
+# xrandr --rate 120
+
+# For Virtual Machine 
+# xrandr --output Virtual-1 --mode 1920x1080
+
+# Keyboard layout
+setxkbmap us
+
+# Load picom
+picom -b
+
+# Load power manager
+xfce4-power-manager &
+
+# launch top bar
+/home/kevin/.config/eww/bar/launch_bar_startup
+
+# launch daemon for eww dashboard
+eww --config /home/kevin/.config/eww/dashboard daemon
+
+# Load notification service
+dunst &
+
+# Setup Wallpaper and update colors
+~/.config/qtile/scripts/wallpaper.sh init
+
+# Load music into player
+~/.config/qtile/scripts/add-music.sh
+
+# Load screensavers (must be after wallpapers)
+xscreensaver --no-splash
